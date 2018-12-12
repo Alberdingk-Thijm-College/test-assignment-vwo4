2d9f43af8ca26bc50c545ab049dd259a88d7c5bd
Timo Overbeek <timotheusoverbeek@gmail.com>
2018-12-12 14:25:16 +0100

    Update opdracht1.ino
    Lampje in een loop gezet

--- opdracht1/opdracht1.ino
+++ opdracht1/opdracht1.ino
@@ -1,11 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-
+  digitalWrite(13, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
   print("Hello World");
+  for(int i = 0; i < 5; i++)
+  {
   digitalWrite(13, HIGH);
+  }
 }
