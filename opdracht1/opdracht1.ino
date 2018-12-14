f93816acbf120bf477196c0ad3be7ed0785de684
Timo Overbeek <timotheusoverbeek@gmail.com>
2018-12-14 10:52:30 +0100

    3e stoplicht
    Ik heb een 3e stoplicht toegevoegd. Stoplicht heeft een rood een groen lampje.

--- opdracht1/opdracht1.ino
+++ opdracht1/opdracht1.ino
@@ -13,6 +13,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   
   print("Hello World");
+  print("Dit is een voorbeeld!");
   for(int i = 0; i < 5; i++)
   {
   digitalWrite(13, HIGH);
